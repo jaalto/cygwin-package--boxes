ceb8c5225f4765d56c6cc4a1cd54b38b84708dae
Jari Aalto <jari.aalto@cante.net>
2011-02-12 00:07:55 +0200

    build.sh: (BVERSION): add. (CFLAGS): Add -ansi -O

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@
 echo "FIXME: sys/stat.h defines S_IFMT and S_IFDIR"
 
 make GLOBALCONF=/etc/boxes/boxes.conf \
+     BVERSION=1.0.1a \
      CFLAGS_ADDTL= \
-     CFLAGS="-I. -Iregexp -W -Wall -DS_IFMT=0170000 -DS_IFDIR=0040000"
+     CFLAGS="-ansi -I. -Iregexp -W -Wall -O -DS_IFMT=0170000 -DS_IFDIR=0040000"
 
 # End of file
