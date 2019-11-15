074fa785bca82c6ef8f1b1431fae65648cb3f997
Jari Aalto <jari.aalto@cante.net>
2019-11-15 09:47:37 +0200

    1.3
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@
 echo "FIXME: sys/stat.h defines S_IFMT and S_IFDIR"
 
 make GLOBALCONF=/etc/boxes/boxes.conf \
-     BVERSION=1.0.1a \
+     BVERSION=1.3 \
      CFLAGS_ADDTL= \
-     CFLAGS="-ansi -I. -Iregexp -W -Wall -O -DS_IFMT=0170000 -DS_IFDIR=0040000"
+     CFLAGS="-D_BSD_SOURCE -std=c11 -I. -Iregexp -W -Wall -O -DS_IFMT=0170000 -DS_IFDIR=0040000"
 
 # End of file
